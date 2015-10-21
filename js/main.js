45433376794c8549af72092df06b669b3f19a070
jonathan elbom <jonathan.elbom@pearson.com>
2015-10-21 11:13:21 -0500

    change row height based on viewport width

--- js/main.js
+++ js/main.js
@@ -29,7 +29,12 @@
 	var $imageHolder;
 	var hasGrid = false;
 	var imageTimeoutId;
-
+	var isMobile = false; //initiate as false
+	// device detection
+	if ( /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+	    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+		isMobile = true;
+	}
 	//get user id and kickoff site
 	getUserId("mikedparsons");
 	// define input event
@@ -47,11 +52,20 @@
 	});
 	var debouncedResize = _.debounce( function() {
 		sizeToScreen();
+		if ( !isMobile ) {
+			updateRowHeight();
+		}
 	}, 200 );
 	$(window).on('resize', function() {
 		$fullsize.removeClass('full-width full-height');
 		debouncedResize();
-	});
+	})
+	if ( isMobile ) {
+		$(window).on('orientationchange', function() {
+			updateRowHeight();
+		});
+	}
+
 	//$loader.hide();
 	// on nav link click
 	$('header').on( mouseEvent, '.nav-item', function() {
@@ -59,7 +73,7 @@
 		var id = $(this).addClass('selected').data('sectionId');
 		$('.section').empty();
 		var photos = id === 'all' ? allPhotos : _.where(allPhotos, {photosetId: id})
-		console.log('photos:',photos);
+		//console.log('photos:',photos);
 		addPhotos( photos );
 
 		initGrid();
@@ -67,7 +81,7 @@
 		$('.navbar-collapse').removeClass('in');
 	});
 
-	$('.thumbs').css('padding-top', $('.main-nav').innerHeight() - 4 + 'px');
+	$('.thumbs').css('padding-top', $('.main-nav').outerHeight() - 1 + 'px');
 	// modal controls position
 	var height = $modalControls.outerHeight();
 	$modalControls.css('top', 'calc(50% - '+height/2+'px)');
@@ -208,7 +222,7 @@
 			$('.section').justifiedGallery().off();
 		}
 		$('.section').justifiedGallery({
-			rowHeight : 300,
+			rowHeight : getRowHeight(), //300,
 		    lastRow : 'nojustify',
 		    margins : 5,
 		    sizeRangeSuffixes: {
@@ -233,8 +247,7 @@
         };
     }
     function toggleLoader( show ) {
-    	console.log('toggleLoader, show:',show);
-    	    	if ( !_.isBoolean(show) ) {
+    	if ( !_.isBoolean(show) ) {
     		show = !$loader.is(":visible");
     	}
     	//$loader.show();
@@ -258,6 +271,20 @@
 			delete imageLoadHash[id];
 			//checkForAllImagesLoaded();
 		}
+	}
+	function getRowHeight() {
+		var vp = getViewportSize();
+		var rowHeight = 300;
+		if (vp.width <= 500) {
+			rowHeight = 100;
+		} else if (vp.width <= 800) {
+			rowHeight = 200;
+		}
+		//console.log('rowHeight:',rowHeight);
+		return rowHeight;
+	}
+	function updateRowHeight() {
+		$('.section').justifiedGallery( {rowHeight:getRowHeight()});
 	}
 	function sizeToScreen() {
 		var vp = getViewportSize();
